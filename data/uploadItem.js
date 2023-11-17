c0818b3d4e8b2efd0275b20109f2f4f6a83fb7a4
allen93075 <allen.lee@ecloudvalley.com>
2023-11-17 05:50:02 -0500

    test for pre-commit

--- data/uploadItem.js
+++ data/uploadItem.js
@@ -2,14 +2,11 @@ import validation from "./validation.js";
 import { users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 
-const usersMethods                              = {
-    async uploadItem                     () {
-
-        console.log(                                               "JJJJ"   )
-        console.log(                   "CCCCCCCCCCCCCC        "                                  )
-
-    }
-}
-
+const usersMethods = {
+  async uploadItem() {
+    console.log("JJJJ");
+    console.log("CCCCCCCCCCCCCC        ");
+  },
+};
 
 export default uploadItem;

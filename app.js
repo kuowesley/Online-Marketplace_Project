034c97d445188b56047e403800f3ae317dc9c078
allen93075 <allen.lee@ecloudvalley.com>
2023-11-17 22:28:05 -0500

    update formatting with prettier

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import constructorMethod from './routes/index.js';
+import express from "express";
+import constructorMethod from "./routes/index.js";
 
 const app = express();
 
@@ -12,5 +12,5 @@ constructorMethod(app);
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log("Your routes will be running on http://localhost:3000");
 });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-import users from './users.js';
+import users from "./users.js";
 
 const constructorMethod = (app) => {
-  app.use('/users', users);
+  app.use("/users", users);
 
-  app.use('*', (req, res) => {
-    return res.status(404).json({error: 'Not found'});
+  app.use("*", (req, res) => {
+    return res.status(404).json({ error: "Not found" });
   });
 };
 

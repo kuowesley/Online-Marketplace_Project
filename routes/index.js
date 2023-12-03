744b944c2ba53d5f7f7c35bf2299e1ecbdeb0ca0
allen93075 <allen.lee@ecloudvalley.com>
2023-12-03 12:19:23 -0500

    add items routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import users from "./users.js";
+import items from "./items.js";
 
 const constructorMethod = (app) => {
   app.use("/users", users);
+  app.use("/items", items);
 
   app.use("*", (req, res) => {
     return res.status(404).json({ error: "Not found" });

ff3c4b21b1d13b8d0828ce4ab6093ce08cac28ff
allen93075 <allen.lee@ecloudvalley.com>
2023-12-03 12:15:01 -0500

    add cookie,multer,static file/path,basic view,debugging function

--- app.js
+++ app.js
@@ -1,12 +1,69 @@
 import express from "express";
 import constructorMethod from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import exphbs from "express-handlebars";
+import session from "express-session";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import multer from "multer";
 
+// set up express server
 const app = express();
 
 // For parsing application/json
 app.use(express.json());
+
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+// config cookie
+app.use(cookieParser());
+
+// config for image, Set up Multer
+let upload = multer({ dest: "upload/" });
+let type = upload.array("fileInput");
+
+// set up static file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticDir = express.static(__dirname + "/public");
+app.use("/public", staticDir);
+
+// config views
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// TODO: Set up session, Cookie expire time
+app.use(
+  session({
+    name: "AuthState",
+    secret: "some secret string!",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+// ---------------------------------------------
+// TODO Log out method and url for debugging, remove after complete
+// set up varible
+let currentTimestamp, requestMethod, requestRoute;
+// middleware start
+// every requests
+app.use("/", type, async (req, res, next) => {
+  // Current Timestamp: new Date().toUTCString()
+  currentTimestamp = new Date().toUTCString();
+  // Request Method: req.method
+  requestMethod = req.method;
+  // Request Route: req.originalUrl
+  requestRoute = req.originalUrl;
+  console.log(`[${currentTimestamp}]: ${requestMethod} ${requestRoute}`);
+  next();
+});
+// ---------------------------------------------
+
+/**
+TODO: Middleware
+ */
 
 constructorMethod(app);
 
